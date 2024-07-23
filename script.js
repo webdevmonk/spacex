a934a68c2de9e065bdeed3185827d3eb769f686f
webdevmonk <you@example.com>
2024-07-23 19:31:21 +0530

    more animations

--- script.js
+++ script.js
@@ -4,5 +4,22 @@ const menuBtn = document.querySelector(".menu-btn");
 menuBtn.addEventListener("click", function () {
   menu.classList.toggle("open");
   menuBtn.classList.toggle("open");
+  document.body.classList.toggle("stop-scroll");
   console.log(menuBtn);
+});
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    entries.forEach(function (section) {
+      console.log("working");
+      if (section.isIntersecting) {
+        section.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.8 },
+);
+
+document.querySelectorAll("section").forEach(function (section) {
+  observer.observe(section);
 });
